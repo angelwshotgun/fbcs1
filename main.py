e91dc954412cddbeed806ee6b420533fa3666603
angelwshotgun <115882828+angelwshotgun@users.noreply.github.com>
2024-07-16 05:11:57 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 from github import Github
 import os
 from io import StringIO
